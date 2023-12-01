a71b78f883acd847ca5a3424f5b193c535484866
league <league@imac-28.attlocal.net>
2023-11-30 19:59:16 -0800

    Finished Into To ArrayLists

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,21 +1,49 @@
 package _00_Intro_To_ArrayLists;
 
+import java.util.ArrayList;
+
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
         // 1. Create an array list of Strings
         //    Don't forget to import the ArrayList class
-
+    	ArrayList<String> list = new ArrayList<String>();
         // 2. Add five Strings to your list
-
+    	list.add("Meddle");
+    	list.add("Dark Side of the Moon");
+    	list.add("Wish You Were Here");
+    	list.add("Animals");
+    	list.add("The Wall");
         // 3. Print all the Strings using a standard for-loop
-
+    	for (int i = 0; i < list.size(); i++)
+    	{
+    		System.out.println(list.get(i));
+    	}
+    	System.out.print("\n");
         // 4. Print all the Strings using a for-each loop
-
+    	for (String s : list)
+    	{
+    		System.out.println(s);
+    	}
+    	System.out.print("\n");
         // 5. Print only the even numbered elements in the list.
-
+    	for (int i = 1; i < list.size(); i += 2)
+    	{
+    		System.out.println(list.get(i));
+    	}
+    	System.out.print("\n");
         // 6. Print all the Strings in reverse order.
-
+    	for (int i = list.size() - 1; i <= 0; i --)
+    	{
+    		System.out.println(list.get(i));
+    	}
+    	System.out.print("\n");
         // 7. Print only the Strings that have the letter 'e' in them.
-        
+        for (String s : list)
+        {
+        	if (s.contains("e"))
+        	{
+        		System.out.println(s);
+        	}
+        }
     }
 }
--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,15 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -13,5 +22,49 @@ public class _02_GuestBook {
      * Guest #3: Greg Ganders
      * Guest #4: Donny Doners
      */
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton AddName = new JButton();
+	JButton ViewNames = new JButton();
+	
+	ArrayList<String> Guests = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		new _02_GuestBook().run();
+	}
+	
+	void run()
+	{
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(AddName);
+		panel.add(ViewNames);
+		
+		AddName.addActionListener(this);
+		AddName.setText("Add Name");
+		ViewNames.addActionListener(this);
+		ViewNames.setText("View Names");
+		
+		frame.pack();
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getSource() == AddName)
+		{
+			String name = JOptionPane.showInputDialog("Add A Name:");
+			Guests.add(name);
+		}
+		else if (arg0.getSource() == ViewNames)
+		{
+			String Message = "Guests: \n";
+			for (int i = 0; i < Guests.size(); i ++)
+			{
+				Message = Message + "Guest #" + i + ": " + Guests.get(i) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, Message);
+		}
+	}
 }
